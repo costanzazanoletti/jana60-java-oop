965f8055c580b9b63b468396cf781e1913de2690
costanzazanoletti <zanoletti.cos@gmail.com>
2022-06-17 07:56:06 +0200

    add scanner.close()

--- src/jana60/shop/Catalogo.java
+++ src/jana60/shop/Catalogo.java
@@ -7,31 +7,31 @@ public class Catalogo {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     // creo l'array vuoto di Prodotto
-    // Prodotto[] catalogo = new Prodotto[3];
+    Prodotto[] catalogo = new Prodotto[3];
 
-    Prodotto nike = new Prodotto("Nike", "Air", 200.0, 22);
-    Prodotto iphone = new Prodotto("Apple", "iphone", 799.0, 22);
-    Prodotto fenix = new Prodotto("Garmin", "Fenix", 898.0, 22);
-
-    Prodotto[] catalogo = {nike, iphone, fenix};
+    // Prodotto nike = new Prodotto("Nike", "Air", 200.0, 22);
+    // Prodotto iphone = new Prodotto("Apple", "iphone", 799.0, 22);
+    // Prodotto fenix = new Prodotto("Garmin", "Fenix", 898.0, 22);
+    //
+    // Prodotto[] catalogo = {nike, iphone, fenix};
 
 
 
     // valorizzo l'array di Prodotto
-    // for (int i = 0; i < catalogo.length; i++) {
-    // // chiedo all'utente i dettagli del prodotto
-    // System.out.print("Inserisci marca: ");
-    // String marca = scanner.nextLine();
-    // System.out.print("Inserisci nome: ");
-    // String nome = scanner.nextLine();
-    // System.out.print("Inserisci prezzo: ");
-    // double prezzo = Double.parseDouble(scanner.nextLine());
-    // System.out.print("Inserisci iva: ");
-    // int iva = Integer.parseInt(scanner.nextLine());
-    //
-    // // con quei dettagli creo un Prodotto e lo assegno alla posizione corrente
-    // catalogo[i] = new Prodotto(marca, nome, prezzo, iva);
-    // }
+    for (int i = 0; i < catalogo.length; i++) {
+      // chiedo all'utente i dettagli del prodotto
+      System.out.print("Inserisci marca: ");
+      String marca = scanner.nextLine();
+      System.out.print("Inserisci nome: ");
+      String nome = scanner.nextLine();
+      System.out.print("Inserisci prezzo: ");
+      double prezzo = Double.parseDouble(scanner.nextLine());
+      System.out.print("Inserisci iva: ");
+      int iva = Integer.parseInt(scanner.nextLine());
+
+      // con quei dettagli creo un Prodotto e lo assegno alla posizione corrente
+      catalogo[i] = new Prodotto(marca, nome, prezzo, iva);
+    }
 
     System.out.println("Il nostro catalogo");
 
@@ -49,6 +49,7 @@ public class Catalogo {
       System.out.println(infoProdotto);
     }
 
+    scanner.close();
 
   }
 
