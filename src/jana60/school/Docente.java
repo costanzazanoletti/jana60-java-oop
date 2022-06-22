ccead816aca90c5e06dac9140181f5a23e62b00f
costanzazanoletti <zanoletti.cos@gmail.com>
2022-06-22 13:26:35 +0200

    ereditarieta school

---
+++ src/jana60/school/Docente.java
@@ -0,0 +1,29 @@
+package jana60.school;
+
+public class Docente extends Persona {
+  // attributi
+  private String materia;
+
+  // costruttori
+  public Docente(String nome, String cognome, String materia) {
+    super(nome, cognome);
+    this.materia = materia;
+  }
+
+  public String getMateria() {
+    return materia;
+  }
+
+  public void setMateria(String materia) {
+    this.materia = materia;
+  }
+
+  @Override
+  public String toString() {
+
+    return super.toString() + " insegnante di " + materia;
+  }
+
+
+
+}
---
+++ src/jana60/school/DocentePrecario.java
@@ -0,0 +1,13 @@
+package jana60.school;
+
+public class DocentePrecario extends Docente {
+  private int numeroContratto;
+
+  public DocentePrecario(String nome, String cognome, String materia) {
+    super(nome, cognome, materia);
+
+  }
+
+
+
+}
