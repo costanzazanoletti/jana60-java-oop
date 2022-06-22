88cc8429cb2eeea91f672472178db0c879c5ae12
costanzazanoletti <zanoletti.cos@gmail.com>
2022-06-22 11:56:55 +0200

    ereditarietà - piattaforma streaming

---
+++ src/jana60/streaming/Main.java
@@ -0,0 +1,21 @@
+package jana60.streaming;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Contenuto spiderMan = new Contenuto("Spider Man");
+    spiderMan.riproduci();
+
+    Movie avengers = new Movie("Avengers");
+    avengers.setDurata(150L);
+
+    avengers.riproduci();
+
+    Serie squidGame = new Serie("Squid Game", 10);
+    System.out.println(squidGame.toString());
+    squidGame.riproduci();
+
+    Serie casaDiCarta = new Serie("La casa di carta", 50);
+  }
+
+}
